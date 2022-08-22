f162189f20874ed74a505b38ed09b224bb20fbc2
eboileau <boileau@uni-heidelberg.de>
2022-08-22 13:29:49 +0200

    MNT bump to major version 2.0.0

--- tests/test_gtf_utils.py
+++ tests/test_gtf_utils.py
@@ -7,8 +7,8 @@
 import numpy as np
 import pandas as pd
 
-import pbio.utils.bed_utils as bed_utils
-import pbio.utils.gtf_utils as gtf_utils
+import pbiotools.utils.bed_utils as bed_utils
+import pbiotools.utils.gtf_utils as gtf_utils
 
 import pandas.util.testing
 import pytest
@@ -16,11 +16,18 @@ import pytest
 
 @pytest.fixture
 def expected_gtf_entries():
-    def get_expected_gtf_feature(start:int, end:int, frame, feature:str,
-            attributes:str=("gene_id my_gene_id; "
-            "my_other_attribute \'my other value\'; "
+    def get_expected_gtf_feature(
+        start: int,
+        end: int,
+        frame,
+        feature: str,
+        attributes: str = (
+            "gene_id my_gene_id; "
+            "my_other_attribute 'my other value'; "
             "my_numeric_attribute 10; "
-            "transcript_id my_transcript;")):
+            "transcript_id my_transcript;"
+        ),
+    ):
         exon = {
             "seqname": "chr1",
             "source": "my_source",
@@ -30,7 +37,7 @@ def expected_gtf_entries():
             "score": 0,
             "frame": frame,
             "strand": "+",
-            "attributes": attributes
+            "attributes": attributes,
         }
 
         return pd.Series(exon)
@@ -43,10 +50,10 @@ def expected_gtf_entries():
         get_expected_gtf_feature(start, end, ".", "exon") for start, end in it
     ]
 
-    expected_gtf_cds_starts  = np.array([21, 51, 81])
+    expected_gtf_cds_starts = np.array([21, 51, 81])
     expected_gtf_cds_ends = np.array([40, 60, 90])
-    #expected_gtf_cds_frame = np.array([0,2,0])
-    #it = zip(expected_gtf_cds_starts, expected_gtf_cds_ends, expected_gtf_cds_frame)
+    # expected_gtf_cds_frame = np.array([0,2,0])
+    # it = zip(expected_gtf_cds_starts, expected_gtf_cds_ends, expected_gtf_cds_frame)
     it = zip(expected_gtf_cds_starts, expected_gtf_cds_ends)
 
     expected_gtf_cds = [
@@ -55,30 +62,32 @@ def expected_gtf_entries():
 
     expected_gtf = expected_gtf_exons + expected_gtf_cds
     expected_gtf = pd.DataFrame(expected_gtf)
-    expected_gtf = expected_gtf.sort_values('start')
+    expected_gtf = expected_gtf.sort_values("start")
     expected_gtf = expected_gtf.reset_index(drop=True)
     return expected_gtf[gtf_utils.gtf_field_names]
 
 
 @pytest.fixture
 def bed_entry():
-    return pd.Series({
-        "seqname": "chr1",
-        "start": 10,
-        "end": 100,
-        "id": "my_transcript",
-        "score": 0,
-        "strand": "+",
-        "thick_start": 20,
-        "thick_end": 90,
-        "color": 0,
-        "num_exons": 3,
-        "exon_lengths": "30,10,20",
-        "exon_genomic_relative_starts": "0,40,70",
-        "gene_id": "my_gene_id",
-        "my_other_attribute": "my other value",
-        "my_numeric_attribute": 10
-    })
+    return pd.Series(
+        {
+            "seqname": "chr1",
+            "start": 10,
+            "end": 100,
+            "id": "my_transcript",
+            "score": 0,
+            "strand": "+",
+            "thick_start": 20,
+            "thick_end": 90,
+            "color": 0,
+            "num_exons": 3,
+            "exon_lengths": "30,10,20",
+            "exon_genomic_relative_starts": "0,40,70",
+            "gene_id": "my_gene_id",
+            "my_other_attribute": "my other value",
+            "my_numeric_attribute": 10,
+        }
+    )
 
 
 def test_get_gtf_entries(expected_gtf_entries, bed_entry):
