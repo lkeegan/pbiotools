cf010208a91f7de44e70e2e529ab699fc71e8835
Liam Keegan <liam@keegan.ch>
2023-01-10 11:12:54 +0100

    only test pickle-stan if pystan < 3 available

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -53,13 +53,16 @@ def test_commands_help_arg(list_commands):
         assert subprocess.run([command, "--help"]).returncode == 0
 
 
-# def test_pickle_stan_command_help_arg():
-#     try:
-#         import pystan
-#         have_pystan2 = True
-#     except:
-#         have_pystan2 = False
-#     if have_pystan2:
-#         assert subprocess.run(["pickle-stan", "--help"]).returncode == 0
-#     else:
-#         pytest.skip("pystan2 not found")
+def test_pickle_stan_command_help_arg():
+    # pickle-stan requires pystan < 3
+    # which is not available on all python versions
+    # so this test only runs if the pystan module can be imported
+    try:
+        import pystan
+        have_pystan2 = True
+    except:
+        have_pystan2 = False
+    if have_pystan2:
+        assert subprocess.run(["pickle-stan", "--help"]).returncode == 0
+    else:
+        pytest.skip("pystan2 not found")
